4466457606588a7f8b83ab9964c320c4ed23405a
BlackErik <blac.erik@gmail.com>
2022-06-14 11:31:34 -0600

    finished eight ball

--- Week 3/Project01-Magic-Eight-Ball/app.js
+++ Week 3/Project01-Magic-Eight-Ball/app.js
@@ -1,27 +1,34 @@
+const URL = "https://cs2022-eight-ball.herokuapp.com";
+
 var app = new Vue({
   el: "#app",
   data: {
     message: "",
     prompt: "Ask A Question",
-
-    messageBank: [
-      "Yes",
-      "No",
-      "Maybe",
-      "Ask Again Tomorrow",
-      "Probably",
-      "Probably Not",
-      "Concentrate and ask again",
-    ],
+    messageColor: "",
 
     question: "",
+    answerReady: true,
   },
 
   methods: {
     askQuestion: function () {
-      let nextIndex = Math.floor(Math.random() * this.messageBank.length);
-      let nextResponse = this.messageBank[nextIndex];
-      this.message = nextResponse;
+      // let nextIndex = Math.floor(Math.random() * this.messageBank.length);
+      // let nextResponse = this.messageBank[nextIndex];
+      this.message = "";
+      this.messageColor = "rgba(255,255,255,0)";
+      this.answerReady = false;
+      setTimeout(() => {
+        fetch(URL + "/questions").then((response) => {
+          response.json().then((data) => {
+            console.log(data);
+            this.message = data["answer"];
+            this.messageColor = data["color"];
+            this.answerReady = true;
+          });
+        });
+      }, 1500);
+      // this.message = nextResponse;
       this.prompt = "Ask Another Question";
       this.question = "";
     },
